fd0d7ace52a5044fc9fe537e012d6396dee25a4b
Mugiwara-Abhishek49 <abhishek.nayak49@gmail.com>
2023-06-26 19:53:45 +0530

    update

--- src/components/cards/Table.js
+++ src/components/cards/Table.js
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from "react";
 import { Card } from "antd";
 import { Table } from "antd";
 
-// import "../cards/myDesign";
 const Table2 = () => {
   let [data, setData] = useState();
   const { Column } = Table;
+  let [fig, setFig] = useState(0);
   let api = "https://dummyjson.com/products";
+
+  const changeImg = (images, fig) => {
+    images?.length >= fig ? setFig(fig + 1) : setFig(0);
+    console.log(images, "fig", fig);
+  };
+
   useEffect(() => {
     (async function () {
       let data = await fetch(api).then((res) => res.json());
@@ -14,6 +20,7 @@ const Table2 = () => {
       console.log(data);
     })();
   }, []);
+
   return (
     <div className="container">
       <Card
@@ -34,6 +41,8 @@ const Table2 = () => {
               y: 340,
             }}
             responsive={["xxl", "xl", "lg", "md", "sm", "xs"]}
+            tableLayout={"auto"}
+            rowKey={data?.id}
           >
             <Column title="Product Name" dataIndex="brand" key="brand" />
             <Column title="title" dataIndex="title" key="title" />
@@ -43,13 +52,34 @@ const Table2 = () => {
               dataIndex="description"
               key="description"
             />
-            <Column title="price" dataIndex="price" key="price" />
+            <Column title="price" dataIndex="price" key="price" width="80px" />
             <Column
-              title="discountPercentage"
+              width="100px"
+              title="discount"
               dataIndex="discountPercentage"
               key="discountPercentage"
             />
-            <Column title="stock" dataIndex="stock" key="stock" />
+            <Column title="stock" dataIndex="stock" key="stock" width="80px" />
+            <Column
+              title="images"
+              dataIndex={"images"}
+              key="images"
+              width="100px"
+              render={(image) => (
+                <>
+                  {" "}
+                  <img
+                    onClick={() => {
+                      changeImg(image, fig);
+                    }}
+                    height="80px"
+                    width="80px"
+                    src={image[fig]}
+                    alt={"no pics"}
+                  />
+                </>
+              )}
+            />
           </Table>
         </div>
       </Card>
